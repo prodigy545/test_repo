b0582050193dfa3887cfe02821162cbe6b4b4e07
nikgnks <nikolasgiannoukos@gmail.com>
2022-09-11 13:28:22 +0300

    afairesi stis prakseis

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 num1 = input('num 1-> ')
 num2 = input('num 2-> ')
-choice = input("1=Prosthesi: ")
+choice = input("1=Prosthesi: ,2=afairesi: ")
+
 
 if choice == '1':
     print(float(num1) + float(num2))
+elif choice == '2':
+    print(float(num1) - float(num2))
 else:
     print('Not supported.')
